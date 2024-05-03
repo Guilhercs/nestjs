937cd21275b27537b6a1a13315294ac66f984604
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2024-05-03 18:11:20 -0300

    test: UserEntity

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -20,4 +20,20 @@ describe("UserEntity", () => {
     expect(sut.props.password).toEqual(props.password);
     expect(sut.props.createdAt).toEqual(props.createdAt);
   })
+
+
+  it('should return name when getter name is called', () => {
+    expect(sut.props.name).toEqual(props.name)
+  })
+  it('should return email when getter email is called', () => {
+    expect(sut.props.email).toEqual(props.email)
+  })
+  it('should return password when getter password is called', () => {
+    expect(sut.props.password).toEqual(props.password)
+  })
+  it('should return createdAt when getter createdAt is called', () => {
+    expect(sut.props.createdAt).toEqual(props.createdAt)
+  })
+
+
 })
