04c8506a4e2b3973b273615f12d15b7267b4a712
Guilherme Carvalho <guilherme.carvalhos@outlook.com>
2024-05-11 15:23:47 -0300

    test: abtract class tests

--- src/shared/domain/entities/__tests__/unit/entity.spec.ts
+++ src/shared/domain/entities/__tests__/unit/entity.spec.ts
@@ -12,7 +12,30 @@ describe("Entity", () => {
   it('Should set props and id', () => {
     const props = { prop1: 'value1', prop2: 15}
     const entity = new stubEntity(props);
-    expect(entity.props).toStrictEqual(props)
+
+    expect(entity.props).toStrictEqual(props);
+    expect(entity._id).not.toBeNull();
+    expect(uuidValidate(entity._id)).toBeTruthy();
   })
+
+  it('should convert a entity to a JSON', () => {
+  const props = { prop1: 'value1', prop2: 12 };
+  const id = '8a58ae47-dc2a-4187-a8be-1d88cabdf040';
+  const entity = new stubEntity(props, id);
+
+  expect(entity._id).toEqual(id)
+  })
+
+  it('should convert a entity to a JSON', () => {
+    const props = { prop1: 'value1', prop2: 12 };
+    const id = '8a58ae47-dc2a-4187-a8be-1d88cabdf040';
+    const entity = new stubEntity(props, id);
+
+    expect(entity.toJSON()).toStrictEqual({
+      id, ...props
+    });
+
+  })
+
 
 })
